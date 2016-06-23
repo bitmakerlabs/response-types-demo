3010ab5e66ef8e2a4b173aa0ac32bc5dbeaf252f
Mina Mikhail <mina@fightingtheboss.com>
2016-06-23 18:05:44 -0400

    Add various respons types for Monsters

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,5 +1,14 @@
 class MonstersController < ApplicationController
   def index
     @monsters = Monster.all
+
+    respond_to do |format|
+      format.html
+      format.text { render text: @monsters.pluck(:name).join(" ") }
+      format.json { render json: @monsters }
+      format.csv do
+        render text: @monsters.to_csv
+      end
+    end
   end
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,14 @@
+require 'csv'
+
 class Monster < ActiveRecord::Base
+  def self.to_csv
+    CSV.generate(headers: true) do |csv|
+      # Add the header row to the resulting CSV
+      csv << %w(ID Name Home Creepiness)
+
+      Monster.all.each do |monster|
+        csv << [monster.id, monster.name, monster.home, monster.creepiness]
+      end
+    end
+  end
 end
